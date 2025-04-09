a4ced5d9288c866c1c795b2a85f4d4af95e42fb6
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-04-09 12:24:18 +0300

    Added activity level

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import pyodbc
 from datetime import datetime
 
 class User:
-    def __init__(self, username=None, email=None, password=None, gender=None, age=None, weight=None, height=None, goal=None):
+    def __init__(self, username=None, email=None, password=None, gender=None, age=None, weight=None, height=None, goal=None, activity_level=None):
         self.username = username
         self.email = email
         self.password = password
@@ -13,6 +13,8 @@ class User:
         self.weight = weight
         self.height = height
         self.goal = goal
+        self.activity_level = activity_level
+
         self.connection_string = ('DRIVER={ODBC Driver 17 for SQL Server};SERVER=localhost;DATABASE=CalorieDiary;Trusted_Connection=yes')
         self.conn = pyodbc.connect(self.connection_string)
         self.cursor = self.conn.cursor()
